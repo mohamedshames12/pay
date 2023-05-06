1a5c106f75550fea94f56fcb8ce4302d280897aa
mohamed shams <mohamedhames900@gmail.com>
2023-05-06 20:46:37 +0300

    update

---
+++ starter-php/server/public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+  require '';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Stripe Sample</title>
+
+    <link rel="stylesheet" href="/css/base.css" />
+  </head>
+  <body>
+    <main>
+      <h1>Stripe Sample</h1>
+    </main>
+  </body>
+</html>
